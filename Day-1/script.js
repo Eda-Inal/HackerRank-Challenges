3a82a947aaf5f816e1aa2ac73b2460891d16b9ad
Eda Işıl İnal <119332810+Eda-Inal@users.noreply.github.com>
2023-03-10 16:07:24 +0300

    Update script.js

--- Day-1/script.js
+++ Day-1/script.js
@@ -47,10 +47,15 @@ function main() {
     
 
 
-if(N % 2 == 0 && 2<=N && N<=5 || N>20){
+if(N % 2 !==0  ){
+    console.log("Weird")
+}
+if(N % 2 == 0 && N>20){
+    console.log("Not Weird")
+} else if(N%2 ==0 && N >=2 && N<=5){
     console.log("Not Weird")
 }
- if( (N % 2 !==0) || (N % 2 ==0) && N>=6 && N<= 20 ){
+if(N %2 == 0 && N>=6 && N<=20){
     console.log("Weird")
 }
 
