bf58fc4599e0abaa3a407c352877475b7d13d74c
Eda Işıl İnal <119332810+Eda-Inal@users.noreply.github.com>
2023-03-21 15:45:14 +0300

    Update script.js

--- Day-1/script.js
+++ Day-1/script.js
@@ -184,3 +184,16 @@ function getSecondLargest(nums) {
     
 
 
+******************************************Day 3: Try, Catch, and Finally*****************************
+
+function reverseString(s) {
+    try {
+      
+       console.log(s.split("").reverse().join(""))
+    } catch(error){
+        console.log(error.message);
+        console.log(s)
+    }
+}
+
+
